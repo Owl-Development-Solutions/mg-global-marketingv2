432dc1ba2728a246ae1ed1520726f8999f346396
Daffodility123 <manuelmorden987@gmail.com>
2025-12-19 16:42:16 +0800

    Update profile card and tabs

--- frontend/src/projects/app/components/profile-card/profile-card.component.ts
+++ frontend/src/projects/app/components/profile-card/profile-card.component.ts
@@ -24,9 +24,9 @@ export class ProfileCardComponent {
     'PMA Reminder for FA049950: Qualified for next month’s benefits...';
 
   balances = [
-    { amount: '339.18P PHP', label: 'PHP Balance' },
-    { amount: '145.00 PHP', label: 'EC Wallet' },
-    { amount: '0', label: 'Travel GC' },
+    { amount: 'PHP', label: 'Balance' },
+    { amount: 'PHP', label: 'EC Wallet' },
+    { amount: 'PHP', label: 'Travel GC' },
   ];
 
   imgUrl = 'person-img.jpg';
