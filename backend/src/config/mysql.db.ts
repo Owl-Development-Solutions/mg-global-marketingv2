4fc1ca712f19086bbdfa367fbbb8cd5b46f533a4
Jabame731 <94708537+Jabame731@users.noreply.github.com>
2026-03-21 21:17:30 +0800

    Update MySQL connection configuration variables

--- backend/src/config/mysql.db.ts
+++ backend/src/config/mysql.db.ts
@@ -15,11 +15,11 @@ let pool: mysql.Pool;
 export const connection = () => {
   if (!pool) {
     pool = mysql.createPool({
-      host: process.env.DB_HOST!,
-      user: process.env.DB_USER!,
-      password: process.env.DB_PASSWORD!,
-      database: process.env.DB_DATABASE!,
-      port: Number(process.env.DB_PORT),
+      host: process.env.DB_HOST_NEW!,
+      user: process.env.DB_USER_NEW!,
+      password: process.env.DB_PASSWORD_NEW!,
+      database: process.env.DB_DATABASE_NEW!,
+      port: Number(process.env.DB_PORT_NEW),
       ssl: {
         rejectUnauthorized: false,
       },
